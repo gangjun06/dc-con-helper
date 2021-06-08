7f7c09d165c8f883771a283d489e1fd633d02185
Gangjun Lee <me@gangjun.dev>
2021-06-08 23:16:53 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Router, Link, Route } from "svelte-routing";
+	import Nav from "./components/Nav.svelte";
+	import HomePage from "./pages/Home.svelte";
+	import SearchPage from "./pages/Search.svelte";
+	import DetailPage from "./pages/Detail.svelte";
+	export let url = "";
+</script>
+
+<Router url="{url}">
+	<Nav />
+	<div class="container">
+		<Route path="search" component={SearchPage} />
+		<Route path="detail" component={DetailPage} />
+		<Route path="/"><HomePage /></Route>
+	</div>
+</Router>
+
+<style>
+</style>
